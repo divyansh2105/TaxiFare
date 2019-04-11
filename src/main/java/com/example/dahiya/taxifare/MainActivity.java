fed2621e38cfd7a79477e7a77f62ec6a1b081d26
divyansh2105 <41578691+divyansh2105@users.noreply.github.com>
2019-04-11 10:34:27 +0530

    Update MainActivity.java

--- src/main/java/com/example/dahiya/taxifare/MainActivity.java
+++ src/main/java/com/example/dahiya/taxifare/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.dahiya.taxifare;
 
-//PACKAGE NAME
-////        com.example.dahiya.taxifare
-////        APP ID
-////        l8bVqQKz1lQVTHRHqMUY
-////        APP CODE
-////        pF-mZTES66lCUCql1B0QcQ
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
@@ -31,36 +28,44 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
-import com.here.android.mpa.common.GeoCoordinate;
-import com.here.android.mpa.common.OnEngineInitListener;
-import com.here.android.mpa.mapping.Map;
-import com.here.android.mpa.mapping.SupportMapFragment;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.mapquest.android.commoncore.log.L;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener
+//        implements NavigationView.OnNavigationItemSelectedListener
         {
 
     Button go_button;
     ImageView imageView;
     EditText frome,toe,passengerse;
     String from_string,to_string,passengers_string;
+    TextView heading;
 
-    private Map map = null;
-    // map fragment embedded in this activity
-    private SupportMapFragment mapFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+        heading=(TextView)findViewById(R.id.heading);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "font/LobsterTwo-Bold.otf");
+        heading.setTypeface(typeface);
 
 
-
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        //DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 //        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
 //                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 //        drawer.addDrawerListener(toggle);
@@ -69,8 +74,6 @@ public class MainActivity extends AppCompatActivity
 //        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 //        navigationView.setNavigationItemSelectedListener(this);
 
-
-        final TextView txtVw = findViewById(R.id.placeName);
 
 
 
@@ -94,89 +97,91 @@ public class MainActivity extends AppCompatActivity
         imageView.setMaxWidth(width);
         imageView.setMaxHeight(height);
         imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+
+
     }
 
-    private void hidetitlebar()
-    {
-        this.getWindow().getDecorView()
-                .setSystemUiVisibility(
-                        View.SYSTEM_UI_FLAG_FULLSCREEN |
-                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
-                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
-                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
-                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                );
-    }
-
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+//    private void hidetitlebar()
+//    {
+//        this.getWindow().getDecorView()
+//                .setSystemUiVisibility(
+//                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+//                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+//                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+//                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+//                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+//                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                );
+//    }
+//
+//    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    @SuppressWarnings("StatementWithEmptyBody")
+//    @Override
+//    public boolean onNavigationItemSelected(MenuItem item) {
+//        // Handle navigation view item clicks here.
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_camera) {
+//            // Handle the camera action
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_slideshow) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
+//
+//        }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+//        return true;
+//    }
 
     public void go_clicked(View view) {
 
         from_string=frome.getText().toString();
         to_string=toe.getText().toString();
         passengers_string=passengerse.getText().toString();
+        //Log.d("errr","f"+from_string+"to"+to_string+"pa"+passengers_string);
 
-
-//        Intent intent=new Intent(MainActivity.this,Combined.class);
-//        intent.putExtra("from",from_string);
-//        intent.putExtra("to",to_string);
-//        intent.putExtra("passengers",passengers_string);
-        Intent intent=new Intent(MainActivity.this,Testing.class);
+        Intent intent=new Intent(MainActivity.this,Combined.class);
+        intent.putExtra("from",from_string);
+        intent.putExtra("to",to_string);
+        intent.putExtra("passengers",passengers_string);
+//        Intent intent=new Intent(MainActivity.this,SearchBarView.class);
         startActivity(intent);
     }
 
